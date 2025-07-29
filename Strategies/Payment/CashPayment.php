5974cc126f129d84177ddca90aef68d8e4422556
Nima Heidari <nima.heidari.n@gmail.com>
2025-07-29 23:33:14 +0330

    Feat: Add CashPayment Strategy

---
+++ Strategies/Payment/CashPayment.php
@@ -0,0 +1,10 @@
+<?php
+namespace Strategies\Payment;
+
+class CashPayment implements PaymentStrategyInterface
+{
+    public function pay(float $amount): string
+    {
+        return "Paid $amount using Cash.";
+    }
+}
